19ad62643834d695aa02fe2dfaec3c5a0404165d
Scott Lavigne <scottlavigne28@gmail.com>
2018-11-28 16:42:01 -0600

    update liscense short hand in package.json and remove silly test

--- tests/tests.ts
+++ tests/tests.ts
@@ -3,7 +3,6 @@ import TestRunner from "../src/TestRunner";
 async function runTests () : Promise<number> {
     try {
         await TestRunner.runTest(`${__dirname}/ImplementedTests/ExampleTest`);
-        await TestRunner.runTest(`${__dirname}/ImplementedTests/ExampleFailTest`);
     } catch (exception) {
         return 1;
     }
